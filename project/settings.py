9b601191c9243e5e5729f5ae1be11f56938b1667
Mahmoud12501 <mahmoudmuhmmed2001@gmail.com>
2022-08-28 16:45:38 +0200

    add login with username or email_adress

--- project/settings.py
+++ project/settings.py
@@ -135,4 +135,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL='/accounts/profile'
+AUTHENTICATION_BACKENDS=['accounts.backends.EmailBackend']
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
